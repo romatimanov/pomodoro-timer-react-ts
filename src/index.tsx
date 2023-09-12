e64734057ad61d72b6a51b7d474030140bc7b151
Roma Timanov <romatimanov77@gmail.com>
2023-09-12 17:14:28 +0700

    ок

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <ThemeStyles />
         <Header />
         <Layout>
